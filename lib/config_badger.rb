e73a6dc28fe7b689fdf41ae9f47ba44cd239e1d0
Rein Henrichs <rein@phpfog.com>
2011-09-26 16:01:03 -0700

    ConfigBadger.[]

--- lib/config_badger.rb
+++ lib/config_badger.rb
@@ -4,6 +4,7 @@ module ConfigBadger
   autoload :VERSION, 'options_badger/version'
 
   class ConfigError    < RuntimeError; end
+  class OptionsError   < ConfigError;  end
   class ConfigNotFound < ConfigError;  end
   class EnvNotFound    < RuntimeError; end
 
@@ -15,4 +16,25 @@ module ConfigBadger
     @options = options
   end
 
+  def self.[](name)
+    assert_options! :path, :env
+
+    file = name + '.yml'
+
+    config_file = File.join(options[:path], file)
+
+    raise ConfigNotFound, "config not found `#{name}` in #{config_file}" unless File.exists?(config_file)
+
+    config = YAML.load_file(config_file)[options[:env]]
+
+    raise EnvNotFound, "env not found `#{options[:env]}` in #{config_file}" unless config
+
+    return config
+  end
+
+  def self.assert_options!(*names)
+    names.each do |name|
+      raise OptionsError, "must specify options[#{name.inspect}]" unless options[name]
+    end
+  end
 end
--- spec/config_badger_spec.rb
+++ spec/config_badger_spec.rb
@@ -18,4 +18,58 @@ describe ConfigBadger do
       ConfigBadger.options.should be_equal(options)
     end
   end
+
+  describe ".[]" do
+    before do
+      ConfigBadger.options = { :path => 'config', :env => 'test' }
+      File.stubs(:exists?).returns true
+    end
+
+    let(:file_path) { File.join('config', 'file.yml') }
+    subject { klass.[]('file') }
+
+    it "should load the YAML file from the path specified in options" do
+      YAML.expects(:load_file).with(file_path).returns({'test' => {}})
+      subject
+    end
+
+    it "should should add the .yml extension" do
+      YAML.expects(:load_file).with(file_path).returns({'test' => {}})
+      klass.[]('file')
+    end
+
+    context "when the file is unavailable" do
+      before { File.expects(:exists?).returns false }
+
+      it "should raise an error" do
+        lambda { subject }.should raise_error ConfigBadger::ConfigNotFound
+      end
+    end
+
+    context "when the file does not contain the Rails environment" do
+      before { YAML.expects(:load_file).with(file_path).returns({'nonesuch' => {}}) }
+
+      it "should raise an error" do
+        lambda { subject }.should raise_error ConfigBadger::EnvNotFound
+      end
+    end
+
+    context "when there is no :path option" do
+      before { ConfigBadger.options = { :env => 'test' } }
+
+      it "should raise an error" do
+        lambda { subject }.should raise_error ConfigBadger::OptionsError
+      end
+    end
+
+    context "when there is no :env option" do
+      before { ConfigBadger.options = { :path => 'test' } }
+
+      it "should raise an error" do
+        lambda { subject }.should raise_error ConfigBadger::OptionsError
+      end
+    end
+
+  end
+
 end
